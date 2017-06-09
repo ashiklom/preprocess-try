4b7c6efe0879cef22f78b4dad70e7da51f0450b2
Alexey Shiklomanov <ashiklom@bu.edu>
2017-06-09 17:17:32 -0400

    Leaf type by family

--- leaf_type.R
+++ leaf_type.R
@@ -23,10 +23,36 @@ lt_proc <- inner_join(lt_long, lt_map)
 lt_species <- lt_proc %>% 
     filter(leaf_type != 'n/a') %>% 
     group_by(AccSpeciesID) %>% 
-    summarize(leaf_type = most_frequent(leaf_type, c('needleleaf', 'broadleaf')))
+    summarize(leaf_type = most_frequent(leaf_type, c('needle', 'broad')))
 
 count(lt_species, leaf_type, sort = TRUE)
 
+species_phylo <- species %>% 
+    select(-N) %>% 
+    left_join(tbl(trydb, 'species_phylo')) %>% 
+    collect(n = Inf)
+
+lt_missing <- anti_join(species_phylo, lt_species)
+lt_missing %>% 
+    #filter(PlantGroup != 'Acrogymnospermae') %>% 
+    count(Family, sort = TRUE)
+
+needle_families <- c('Pinaceae', 'Cupressaceae')
+broad_families <- c('Poaceae', 'Fabaceae', 'Orchidaceae', 'Myrtaceae', 'Rosaceae', 'Cyperaceae',
+                    'Rubiaceae', 'Iridaceae', 'Lamiaceae', 'Proteaceae', 'Asteraceae',
+                    'Malvaceae', 'Euphorbiaceae')
+
+lt_filled <- lt_missing %>% 
+    mutate(leaf_type = case_when(!is.na(.$Family) & .$Family %in% needle_families ~ 'needle',
+                                 !is.na(.$Family) & .$Family %in% broad_families ~ 'broad',
+                                 TRUE ~ NA_character_)) %>% 
+    filter(!is.na(leaf_type)) %>% 
+    distinct(AccSpeciesID, leaf_type)
+
+lt_species <- lt_species %>% 
+    anti_join(lt_missing) %>% 
+    full_join(lt_filled)
+    
 saveRDS(lt_species, 'attributes/leaf_type.rds')
 
 # Also, may be of interest...
