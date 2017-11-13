6e54211d19040bb709c2c561e9f5a3283ce4c996
Alexey Shiklomanov <ashiklom@bu.edu>
2017-11-13 11:48:35 -0500

    Use double quotes

--- make_pft.R
+++ make_pft.R
@@ -1,65 +1,67 @@
 library(tidyverse)
-match_str <- 'SLA|leaf_lifespan|mass|area'
+match_str <- "SLA|leaf_lifespan|mass|area"
 
-#growth_form_ignore <- c('liana_climber', 'cryptophyte')
-#growth_form_nonwoody <- c('graminoid', 'forb_herb', 'hemicryptophyte',
-                          #'therophyte', 'chamaephyte', 'fern',
-                          #'epiphyte', 'geophyte', 'perennial')
+#growth_form_ignore <- c("liana_climber", "cryptophyte")
+#growth_form_nonwoody <- c("graminoid", "forb_herb", "hemicryptophyte",
+                          #"therophyte", "chamaephyte", "fern",
+                          #"epiphyte", "geophyte", "perennial")
 
-species_phylo <- src_sqlite('try.sqlite') %>% tbl('species_phylo') %>% collect()
+species_phylo <- src_sqlite("try.sqlite") %>% tbl("species_phylo") %>% collect()
 
-plant_attrs_raw <- readRDS('attributes/leaf_type.rds') %>% 
-    full_join(readRDS('attributes/phenology.rds')) %>% 
-    full_join(readRDS('attributes/ps_pathway.rds')) %>% 
-    full_join(readRDS('attributes/growth_form.rds')) %>% 
-    full_join(readRDS('attributes/n_fixation.rds')) %>% 
-    full_join(readRDS('attributes/climate_zone.rds')) %>% 
+plant_attrs_raw <- readRDS("attributes/leaf_type.rds") %>%
+    full_join(readRDS("attributes/phenology.rds")) %>%
+    full_join(readRDS("attributes/ps_pathway.rds")) %>%
+    full_join(readRDS("attributes/growth_form.rds")) %>%
+    full_join(readRDS("attributes/n_fixation.rds")) %>%
+    full_join(readRDS("attributes/climate_zone.rds")) %>%
     left_join(species_phylo)
 
 plant_attrs_raw %>% count(growth_form, sort = TRUE)
 
-woody_gf <- c('woody', 'tree', 'shrub')
+woody_gf <- c("woody", "tree", "shrub")
 
 plant_attrs <- plant_attrs_raw %>% 
-    mutate(woodiness = case_when(!is.na(.$growth_form) & .$growth_form %in% woody_gf ~ 'woody',
-                                 !is.na(.$growth_form) & !(.$growth_form %in% woody_gf) ~ 'nonwoody',
+    mutate(woodiness = case_when(!is.na(.$growth_form) & .$growth_form %in% woody_gf ~ "woody",
+                                 !is.na(.$growth_form) & !(.$growth_form %in% woody_gf) ~ "nonwoody",
                                  TRUE ~ NA_character_))
 
-source('pft_schemes.R')
-pfts <- plant_attrs %>% 
-    rowwise() %>% 
+source("pft_schemes.R")
+pfts <- plant_attrs %>%
+    rowwise() %>%
     mutate(jules1 = jules1_assign(growth_form, ps_pathway, leaf_type),
            jules2 = jules2_assign(growth_form, ps_pathway, leaf_type, phenology, climate_zone),
            clm45 = clm45_assign(growth_form, ps_pathway, leaf_type, phenology, climate_zone),
            custom = custom_assign(growth_form, ps_pathway, woodiness, phenology, leaf_type, n_fixation, climate_zone)) %>% 
-    'class<-'(c('tbl_df', 'data.frame'))
+    "class<-"(c("tbl_df", "data.frame"))
 
-pftcols <- c('jules1', 'jules2', 'clm45', 'custom')
+#pftcols <- c("jules1", "jules2", "clm45", "custom")
+pftcols <- c("clm45")
 
-saveRDS(pfts, 'pfts_species/all_pfts.rds')
+saveRDS(pfts, "pfts_species/all_pfts.rds")
 
-distinct_pfts <- pfts %>% 
-    filter(!is.na(jules1), !is.na(jules2), !is.na(clm45), !is.na(custom)) %>% 
-    distinct(AccSpeciesID, jules1, jules2, clm45, custom)
+distinct_pfts <- pfts %>%
+    filter(!is.na(clm45)) %>%
+    distinct(AccSpeciesID, clm45)
+    #filter(!is.na(jules1), !is.na(jules2), !is.na(clm45), !is.na(custom)) %>%
+    #distinct(AccSpeciesID, jules1, jules2, clm45, custom)
 
-traits_fill <- readRDS('traits/trait_data.rds')
+traits_fill <- readRDS("traits/trait_data.rds")
 
 traits_pfts <- left_join(traits_fill, distinct_pfts)
 
-saveRDS(traits_pfts, file = 'traits/traits_pfts.rds')
+saveRDS(traits_pfts, file = "traits/traits_pfts.rds")
 
-traits_analysis <- traits_pfts %>% 
-    filter_at(vars(one_of(pftcols)), all_vars(!is.na(.))) %>% 
-    filter_at(vars(matches(match_str)), any_vars(!is.na(.))) %>% 
-    select(ObservationID, AccSpeciesID, one_of(pftcols), which(sapply(., is_double)), 
-           -Latitude, -Longitude)
+traits_analysis <- traits_pfts %>%
+    filter_at(vars(one_of(pftcols)), all_vars(!is.na(.))) %>%
+    filter_at(vars(matches(match_str)), any_vars(!is.na(.))) %>%
+    select(ObservationID, AccSpeciesID, one_of(pftcols), which(sapply(., is_double)))
 
-saveRDS(traits_analysis, file = 'traits/traits_analysis.rds')
+saveRDS(traits_analysis, file = "traits/traits_analysis.rds")
 ############################################################
 
 #plant_attrs %>% count(phenology, sort = TRUE)
 
 #plant_attrs %>% filter(is.na(growth_form))
 
-#plant_attrs %>% filter(phenology == 'deciduous', leaf_type == 'needle') %>% count(growth_form)
+#plant_attrs %>% filter(phenology == "deciduous", leaf_type == "needle") %>% count(growth_form)
 
--- pft_schemes.R
+++ pft_schemes.R
@@ -1,24 +1,24 @@
 # JULES 1
-jules1_levels <- c('broadleaf',
-                   'needleleaf',
-                   'shrub',
-                   'c3_grass',
-                   'c4_grass')
+jules1_levels <- c("broadleaf",
+                   "needleleaf",
+                   "shrub",
+                   "c3_grass",
+                   "c4_grass")
 jules1_assign <- function(growth_form, ps_pathway, leaf_type) {
     pft <- NA_character_
-    if (isTRUE(growth_form == 'graminoid')) {
-        if (isTRUE(ps_pathway == 'C4')) {
-            pft <- 'c4_grass'
+    if (isTRUE(growth_form == "graminoid")) {
+        if (isTRUE(ps_pathway == "C4")) {
+            pft <- "c4_grass"
         } else {
-            pft <- 'c3_grass'
+            pft <- "c3_grass"
         }
-    } else if (isTRUE(growth_form == 'shrub')) {
-        pft <- 'shrub'
-    } else if (isTRUE(growth_form == 'tree')) {
-        if (isTRUE(leaf_type == 'needle')) {
-            pft <- 'needleleaf'
-        } else if (isTRUE(leaf_type == 'broad')) {
-            pft <- 'broadleaf'
+    } else if (isTRUE(growth_form == "shrub")) {
+        pft <- "shrub"
+    } else if (isTRUE(growth_form == "tree")) {
+        if (isTRUE(leaf_type == "needle")) {
+            pft <- "needleleaf"
+        } else if (isTRUE(leaf_type == "broad")) {
+            pft <- "broadleaf"
         }
     }
     pft_factor <- factor(pft, levels = jules1_levels)
@@ -26,44 +26,44 @@ jules1_assign <- function(growth_form, ps_pathway, leaf_type) {
 }
 
 # JULES 2
-jules2_levels <- c('broadleaf_evergreen_tropical',
-                   'broadleaf_evergreen_temperate',
-                   'broadleaf_deciduous',
-                   'needleleaf_evergreen',
-                   'needleleaf_deciduous',
-                   'shrub_evergreen',
-                   'shrub_deciduous',
-                   'c3_grass',
-                   'c4_grass')
+jules2_levels <- c("broadleaf_evergreen_tropical",
+                   "broadleaf_evergreen_temperate",
+                   "broadleaf_deciduous",
+                   "needleleaf_evergreen",
+                   "needleleaf_deciduous",
+                   "shrub_evergreen",
+                   "shrub_deciduous",
+                   "c3_grass",
+                   "c4_grass")
 jules2_assign <- function(growth_form, ps_pathway, leaf_type, phenology, climate_zone) {
     pft <- NA_character_
-    if (isTRUE(growth_form == 'graminoid')) {
-        if (isTRUE(ps_pathway == 'C4')) {
-            pft <- 'c4_grass'
+    if (isTRUE(growth_form == "graminoid")) {
+        if (isTRUE(ps_pathway == "C4")) {
+            pft <- "c4_grass"
         } else {
-            pft <- 'c3_grass'
+            pft <- "c3_grass"
         }
-    } else if (isTRUE(growth_form == 'shrub')) {
-        if (isTRUE(phenology == 'deciduous')) {
-            pft <- 'shrub_deciduous'
-        } else if (isTRUE(phenology == 'evergreen')) {
-            pft <- 'shrub_evergreen'
+    } else if (isTRUE(growth_form == "shrub")) {
+        if (isTRUE(phenology == "deciduous")) {
+            pft <- "shrub_deciduous"
+        } else if (isTRUE(phenology == "evergreen")) {
+            pft <- "shrub_evergreen"
         }
-    } else if (isTRUE(growth_form == 'tree')) {
-        if (isTRUE(leaf_type == 'needle')) {
-            if (isTRUE(phenology == 'deciduous')) {
-                pft <- 'needleleaf_deciduous'
+    } else if (isTRUE(growth_form == "tree")) {
+        if (isTRUE(leaf_type == "needle")) {
+            if (isTRUE(phenology == "deciduous")) {
+                pft <- "needleleaf_deciduous"
             } else {
-                pft <- 'needleleaf_evergreen'
+                pft <- "needleleaf_evergreen"
             }
-        } else if (isTRUE(leaf_type == 'broad')) {
-            if (isTRUE(phenology == 'deciduous')) {
-                pft <- 'broadleaf_deciduous'
-            } else if (isTRUE(phenology == 'evergreen')) {
-                if (isTRUE(climate_zone == 'tropical')) {
-                    pft <- 'broadleaf_evergreen_tropical'
-                } else if (isTRUE(climate_zone %in% c('temperate', 'boreal'))) {
-                    pft <- 'broadleaf_evergreen_temperate'
+        } else if (isTRUE(leaf_type == "broad")) {
+            if (isTRUE(phenology == "deciduous")) {
+                pft <- "broadleaf_deciduous"
+            } else if (isTRUE(phenology == "evergreen")) {
+                if (isTRUE(climate_zone == "tropical")) {
+                    pft <- "broadleaf_evergreen_tropical"
+                } else if (isTRUE(climate_zone %in% c("temperate", "boreal"))) {
+                    pft <- "broadleaf_evergreen_temperate"
                 }
             }
         }
@@ -141,55 +141,55 @@ clm45_assign <- function(growth_form, ps_pathway, leaf_type, phenology, climate_
 }
 
 # My custom PFT scheme
-custom_levels <- c('broadleaf_evergreen_tropical',
-                   'broadleaf_evergreen_temperate',
-                   'broadleaf_deciduous_tropical',
-                   'broadleaf_deciduous_temperate',
-                   'needleleaf_evergreen',
-                   'needleleaf_deciduous',
-                   'c3_grass',
-                   # 'c3_forb',
-                   'c4',
-                   'succulent',
-                   'n_fixer')
+custom_levels <- c("broadleaf_evergreen_tropical",
+                   "broadleaf_evergreen_temperate",
+                   "broadleaf_deciduous_tropical",
+                   "broadleaf_deciduous_temperate",
+                   "needleleaf_evergreen",
+                   "needleleaf_deciduous",
+                   "c3_grass",
+                   # "c3_forb",
+                   "c4",
+                   "succulent",
+                   "n_fixer")
 custom_assign <- function(growth_form, ps_pathway, woodiness, phenology, leaf_type, n_fixation, climate_zone) {
     pft <- NA_character_
     # First try based on attributes
     # `isTRUE` is necessary to handle missing values
-    if (isTRUE(growth_form == 'succulent' | ps_pathway == 'CAM')) {
-        pft <- 'succulent'
-    } else if (isTRUE(ps_pathway == 'C4')) {
-        pft <- 'c4'
+    if (isTRUE(growth_form == "succulent" | ps_pathway == "CAM")) {
+        pft <- "succulent"
+    } else if (isTRUE(ps_pathway == "C4")) {
+        pft <- "c4"
     } else if (isTRUE(n_fixation)) {
-        pft <- 'n_fixer'
-    } else if (isTRUE(woodiness == 'woody')) {
-        if (isTRUE(leaf_type == 'broad')) {
-            if (isTRUE(phenology == 'deciduous')) {
-                if (isTRUE(climate_zone == 'tropical')) {
-                    pft <- 'broadleaf_deciduous_tropical'
-                } else if (isTRUE(climate_zone %in% c('temperate', 'boreal'))) {
-                    pft <- 'broadleaf_deciduous_temperate'
+        pft <- "n_fixer"
+    } else if (isTRUE(woodiness == "woody")) {
+        if (isTRUE(leaf_type == "broad")) {
+            if (isTRUE(phenology == "deciduous")) {
+                if (isTRUE(climate_zone == "tropical")) {
+                    pft <- "broadleaf_deciduous_tropical"
+                } else if (isTRUE(climate_zone %in% c("temperate", "boreal"))) {
+                    pft <- "broadleaf_deciduous_temperate"
                 }
-            } else if (isTRUE(phenology == 'evergreen')) {
-                if (isTRUE(climate_zone == 'tropical')) {
-                    pft <- 'broadleaf_evergreen_tropical'
-                } else if (isTRUE(climate_zone %in% c('temperate', 'boreal'))) {
-                    pft <- 'broadleaf_evergreen_temperate'
+            } else if (isTRUE(phenology == "evergreen")) {
+                if (isTRUE(climate_zone == "tropical")) {
+                    pft <- "broadleaf_evergreen_tropical"
+                } else if (isTRUE(climate_zone %in% c("temperate", "boreal"))) {
+                    pft <- "broadleaf_evergreen_temperate"
                 }
             }
-        } else if (isTRUE(leaf_type == 'needle')) {
-            if (isTRUE(phenology == 'deciduous')) {
-                pft <- 'needleleaf_deciduous'
+        } else if (isTRUE(leaf_type == "needle")) {
+            if (isTRUE(phenology == "deciduous")) {
+                pft <- "needleleaf_deciduous"
             } else {
-                pft <- 'needleleaf_evergreen'
+                pft <- "needleleaf_evergreen"
             }
         }
-    } else if (isTRUE(woodiness == 'nonwoody')) {
-        if (isTRUE(growth_form == 'graminoid')) {
-            pft <- 'c3_grass'
+    } else if (isTRUE(woodiness == "nonwoody")) {
+        if (isTRUE(growth_form == "graminoid")) {
+            pft <- "c3_grass"
         }
-        # } else if (isTRUE(growth_form == 'forb')) {
-        #     pft <- 'c3_forb'
+        # } else if (isTRUE(growth_form == "forb")) {
+        #     pft <- "c3_forb"
         # }
     }
     pft_factor <- factor(pft, levels = custom_levels)
